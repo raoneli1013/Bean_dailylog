a9dcb0c92d85d9e118392bc17cb3e465ae11433c
sojin park <psjin977@naver.com>
2023-05-22 17:54:44 +0900

    dairy model add

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+# from user.serializers
 
-# Create your views here.
+
+# class ProfileView(APIView):
+#     def get_object(self, user_id):
+#         return get_object_or_404(User, id=user_id)
+    
