2eb7f6533908fb997a4a80fdf5dad381c56d6513
scarlet0star <scarlet0star@gmail.com>
2023-05-23 22:21:26 +0900

    MODIFY : tasks.py

--- diary/tasks.py
+++ diary/tasks.py
@@ -17,10 +17,10 @@ def create_image_task(user_input):
     prompt = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=gpt_prompt)
     
-    #result = openai.Image.create(prompt=prompt,size="512x512")
+    result = openai.Image.create(prompt=prompt,size="512x512")
     return prompt
 
-#testcode
+# for test
 @shared_task
 def add(x, y):
     return x + y
